48856fa9e7a61412b4bd32f287547c45b04739fc
Francesco Ciulla <francescociullaroma@gmail.com>
2019-09-20 15:22:28 +0200

    dropdowns tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HttpTestComponent } from './http-test/http-test.component';
 import { HomeComponent } from './home/home.component';
+import { DropdownsComponent } from './dropdowns/dropdowns.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     HttpTestComponent,
-    HomeComponent
+    HomeComponent,
+    DropdownsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
